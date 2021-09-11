aab3f4372bbf294a70a3341f1b1a8e78961a11fa
Gilberto Augusto <gilberto.bastos@outlook.com.br>
2021-09-11 18:51:16 -0300

    Iniciada implementação do repositório.

---
+++ src/model/funcionario-model.js
@@ -0,0 +1,10 @@
+class Funcionario {
+  constructor(idade, nome, cargo, id) {
+    this.idade = idade;
+    this.nome = nome;
+    this.cargo = cargo;
+    this.id = id;
+  }
+}
+
+module.exports = Funcionario;
---
+++ src/repository/funcionario-repository.js
@@ -0,0 +1,37 @@
+const Funcionario = require("../model/funcionario-model.js");
+
+class FuncionarioRepository {
+  /**
+   * @param {AWS.DynamoDB}
+   */
+  constructor(awsDocumentClient) {
+    this._awsDocumentClient = awsDocumentClient;
+  }
+
+  async getFuncionarios() {
+    return await this._awsDocumentClient.get({
+      TableName: 'funcionario-crud-funcionario'
+    }).data;
+  }
+
+  async getFuncionarioById(id) {
+    return await this._awsDocumentClient.get({
+      TableName: 'funcionario-crud-funcionario',
+      Key: {
+        "id": id
+      }
+    }).data;
+  }
+
+  async addFuncionario(funcionario) {
+    this._awsDocumentClient.put({
+      Body: JSON.stringify(funcionario)
+    });
+  }
+
+  async updateFuncionario(funcionario) {}
+
+  async deleteFuncionario(id) {}
+}
+
+module.exports = FuncionarioRepository;
